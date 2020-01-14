210734ed9dade68f89e75ba97fb7e3b99bab08a0
h571725 <stefthue58@gmail.com>
2020-01-14 13:24:37 +0100

    onclick til flere veiws

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -4,27 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     EditText editText;
     TextView textView;
+    Boolean aBoolean = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-         editText = findViewById(R.id.edit);
-         textView = findViewById(R.id.text);
-         String string = getIntent().getExtras().getString(MainActivity.MYSTRING);
-         if(string != null){
-             textView.setText(string);
-         }
+        editText = findViewById(R.id.edit);
+        textView = findViewById(R.id.text);
+        String string = getIntent().getExtras().getString(MainActivity.MYSTRING);
+        if (string != null) {
+            textView.setText(string);
+        }
     }
 
     public void onclick(View view) {
-       String text = editText.getText().toString();
-       textView.setText(text);
-       editText.setText("");
+        switch (view.getId()) {
+            case R.id.checkBox:
+                CheckBox checkBox = findViewById(R.id.checkBox);
+                aBoolean = checkBox.isChecked();
+                break;
+            case R.id.button:
+                String text = editText.getText().toString();
+                if (aBoolean) {
+                    text = text.toUpperCase();
+                }
+                textView.setText(text);
+                editText.setText("");
+        }
     }
 }
