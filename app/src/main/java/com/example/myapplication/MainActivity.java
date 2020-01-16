58000f90b14417f8ffca8aed03a1a2d00d57a264
h571725 <stefthue58@gmail.com>
2020-01-16 10:43:11 +0100

    suprise

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 public static final String MYSTRING = "mystring";
@@ -14,10 +18,28 @@ public static final String MYSTRING = "mystring";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String [] array = getResources().getStringArray(R.array.vectorArray);
-        for (String s : array){
-            Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-        }
+        final ImageView sampleImage;
+        final Button show;
+        sampleImage = (ImageView) findViewById(R.id.vector);
+        show = (Button)findViewById(R.id.button1);
+        show.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                sampleImage.setVisibility(View.VISIBLE);
+
+                String [] array = getResources().getStringArray(R.array.vectorArray);
+                for (String s : array){
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                }
+                show.setVisibility(View.GONE);
+
+            }
+        });
+
+
+
     }
 
     public void onclick(View view) {
