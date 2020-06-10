e4299c58c53dea3af2613065b7caa225815d9fbb
h571725 <stefthue58@gmail.com>
2020-06-10 17:04:12 +0200

    Added quotes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,38 +3,42 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String MYSTRING = "mystring";
-    ArrayList<HashMap<String, String>> contactList;
-    private RequestQueue queue;
+    ArrayList<HashMap<String, String>> quoteList;
+
     private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+    TextView random;
+    Button show1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        queue = Volley.newRequestQueue(this);
+        quoteList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
 
-        GetQuotes quotes = new GetQuotes();
-        quotes.execute();
-
-
+        //hide image and button method
         final ImageView sampleImage;
         final Button show;
         sampleImage = findViewById(R.id.vector);
@@ -55,8 +59,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //button for a random quote from office api
+        random = findViewById(R.id.quote);
+        show1 = findViewById(R.id.quoteKnapp);
+        show1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                quoteKnapp();
+            }
+        });
     }
+
+    private void quoteKnapp(){
+        new GetQuotes().execute();
+        random.setVisibility(View.VISIBLE);
+    }
+
 
     public void onclick(View view) {
         Intent intent = new Intent(this, Main2Activity.class);
@@ -65,49 +83,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private class GetQuotes extends AsyncTask<Void, Void, String> {
+private class GetQuotes extends AsyncTask<Void, Void, String> {
 
-        @Override
-        protected String doInBackground(Void... voids) {
-            HttpHandler sh = new HttpHandler();
-            String url = "https://the-office-api.herokuapp.com/season/1/format/quotes";
-            String jsonStr = sh.makeServiceCall(url);
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
 
-            return jsonStr;
-           /* Log.e(TAG, "Response from url: " + jsonStr);
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
+    }
 
-                    // Getting JSON Array node
-                    JSONArray quotes = jsonObj.getJSONArray("quotes");
+    @Override
+    protected String doInBackground(Void... voids) {
+        HttpHandler sh = new HttpHandler();
+        String url = "https://the-office-api.herokuapp.com/season/1/format/quotes";
+        String jsonStr = sh.makeServiceCall(url);
+        Random rand = new Random();
+        int random = rand.nextInt(10);
+
+        //return jsonStr;
+        Log.e(TAG, "Response from url: " + jsonStr);
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                // Getting JSON Array node
+                JSONArray data = jsonObj.getJSONArray("data").getJSONArray(0);
+                for (int i = 0; i < data.length(); i++) {
+                    JSONObject q = data.getJSONObject(i);
+                    String quotes = q.getString("quotes");
 
 
-                    return null;
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
-
+                    HashMap<String, String> list = new HashMap<>();
+                    list.put("quotes", quotes);
+                    quoteList.add(list);
                 }
+
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
 
             }
 
-            return null;*/
-
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            TextView textView = findViewById(R.id.quote);
-            textView.setText(s);
-        }
+        return String.valueOf(quoteList);
+
     }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        //ListAdapter adapter = new SimpleAdapter(MainActivity.this, quoteList,
+           //     R.layout.activity_main, new String[]{ "quote"},
+         //       new int[]{R.id.quote});
+       // lv.setAdapter(adapter);
+       TextView textView = findViewById(R.id.quote);
+       textView.setText(s);
+
+    }
+}
 }
